2a0870d1635392f754f206e4dcbe4effd8db309e
Lenrick Ryan Escasinas <lenrickryanescasinas@gmail.com>
2025-05-20 00:03:46 +0800

    tailoroders, tailorprofile, and so on

--- app/src/main/java/com/example/fitcraft/GarmentApi.kt
+++
@@ -1,8 +0,0 @@
-package com.example.fitcraft
-import retrofit2.http.GET
-import retrofit2.http.Path
-import retrofit2.Call
-interface GarmentApi {
-    @GET("garments/{bodyType}")
-    fun getGarments(@Path("bodyType") bodyType: String): Call<List<Garment>>
-}
